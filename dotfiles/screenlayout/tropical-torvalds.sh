7169ed6aba49d1d8d081750434366eedc41552a4
jnmcfly <jan.bechtel@gmail.com>
2022-12-28 16:59:33 +0100

    dotdrop refactor

---
+++ dotfiles/screenlayout/tropical-torvalds.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+MOOD=$1
+
+case "$(xrandr | grep -wc connected )" in
+  1) bspc monitor %eDP-1 -d sh browser code slack discord lens 1 2 3 4 5;
+    feh --bg-fill "${HOME}/.wallpapers/${MOOD}/primary.jpg";
+  ;;
+  2) ~/.screenlayout/dual.sh;
+    bspc monitor %HDMI-1 -d sh browser code e1 e2 e3 e4 lens && bspc monitor %eDP-1 -d slack discord 1 2 3;
+    feh --bg-fill "${HOME}/.wallpapers/${MOOD}/primary.jpg" "${HOME}/.wallpapers/${MOOD}/secondary.jpg";
+  ;;
+  *) echo default
+  ;;
+esac
